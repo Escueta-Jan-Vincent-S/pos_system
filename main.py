83ddd7409d89b0b333d62e3422fe95d621e11190
Escueta, Jan Vincent S. <escuetajanvincent04@gmail.com>
2026-03-04 00:00:25 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import customtkinter as ctk
-import time
 from controllers import controller
 from settings import APP_NAME
 
@@ -11,7 +10,6 @@ class App(ctk.CTk):
         self.attributes("-fullscreen", True)
         ctk.set_appearance_mode("light")
 
-        # Container to hold all pages in the same spot
         self.container = ctk.CTkFrame(self)
         self.container.pack(fill="both", expand=True)
         self.container.grid_rowconfigure(0, weight=1)
@@ -25,7 +23,6 @@ class App(ctk.CTk):
         self.dashboard_page = DashboardPage(self.container)
         self.user_page = UserPage(self.container)
 
-        # Place them both in the same grid cell
         self.dashboard_page.grid(row=0, column=0, sticky="nsew")
         self.user_page.grid(row=0, column=0, sticky="nsew")
 
@@ -36,20 +33,6 @@ class App(ctk.CTk):
             self.dashboard_page.tkraise()
         elif page_name == "user":
             self.user_page.tkraise()
-
-    def smooth_switch(self, page_name):
-        # Fade out
-        for i in range(10, 6, -1):
-            self.attributes("-alpha", i / 10)
-            self.update()
-            time.sleep(0.01)
-
-        self.show_page(page_name)
-
-        for i in range(7, 11):
-            self.attributes("-alpha", i / 10)
-            self.update()
-            time.sleep(0.01)
 
 if __name__ == "__main__":
     app = App()
